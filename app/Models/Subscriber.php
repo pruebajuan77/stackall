dffaa109035930b78aa5bb81d99b8f1ce6331ec6
pruebajuan77 <pruebajuan77@gmail.com>
2022-03-23 11:42:09 -0500

    Agregando El Modelo Subscriber con su Modelo, Factory, Controller y la Migracion

---
+++ app/Models/Subscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subscriber extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'email',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
